5756031a644526706a441a9dd86583df317b9f93
Dallas Pataska <pataskad@gmail.com>
2021-09-16 16:18:05 -0500

    Created html formatting with css added, created gameBoard module to display game board

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 "use strict"
 
 // the goal is for little global code as possible!
+// the 'Module' pattern wraps the factory in an IIFE
 
 // player object (factory function (IIFE?))
 
 // gameBoard (array) (Module) (IIFE?)
+const gameBoard = (() => {
+    let board = []
+     
+    // 'DOM "cache"'/ quick lookup
+    let gameBoardContainer = document.querySelector('#game-board-container')
+
+    renderDisplay()
+
+    function renderDisplay() {
+        for (let i = 0; i < 9; i++) {
+            board[i] = document.createElement('div')
+            board[i].classList.add('gameBoard-divs')
+            board[i].addEventListener('click', divClick)
+            gameBoardContainer.appendChild(board[i])
+        }
+    }
+    function divClick() {
+        console.log('clicked')
+    }
+    return {
+        renderDisplay: renderDisplay
+    }
+})()
 
 // display controller (to control flow of the game) (Module)
 
