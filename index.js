ef1fcb5aaadffdc2e650579a73e7b6773e3105a6
Dallas Pataska <pataskad@gmail.com>
2021-09-20 14:18:14 -0500

    Working tic tac toe game that places markers within chosen square (only once), with an aside bar to reset the game board included and working as well

--- index.js
+++ index.js
@@ -7,35 +7,58 @@
 
 // gameBoard (array) (Module) (IIFE?)
 const gameBoard = (() => {
-    let board = []  // reads as length of 9
+    let board = []  // reads as length of 9 when rendered
+    let gamePiece = 'X'
      
     // 'DOM "cache"'/ quick lookup
-    let gameBoardContainer = document.querySelector('#game-board-container')
+    const gameBoardContainer = document.querySelector('#game-board-container')
 
     renderDisplay()
 
     function renderDisplay() {
-        clearDisplay()
         for (let i = 0; i < 9; i++) {
             board[i] = document.createElement('div')
             board[i].classList.add('gameBoard-divs')
-            board[i].addEventListener('click', divClick)
+            board[i].id = `${board.indexOf(board[i]) + 1}`
             gameBoardContainer.appendChild(board[i])
         }
     }
-    function divClick() {
-        console.log('clicked')
-    }
-    function clearDisplay() {
-        while (gameBoardContainer.firstChild) {
-            gameBoardContainer.removeChild(gameBoardContainer.firstChild)
+    document.addEventListener('click', (e) => {
+        if (e.target.matches('.gameBoard-divs')) {
+            if (e.target.textContent == '') {
+                if (gamePiece === 'X') {
+                    e.target.textContent = gamePiece
+                    gamePiece = 'O'
+                } else {
+                    e.target.textContent = gamePiece
+                    gamePiece = 'X'
+                }
+            }
         }
-    }
-    return {
-        renderDisplay: renderDisplay
-    }
+    })
+    return {    }
 })()
 
 // display controller (to control flow of the game) (Module)
+const displayController = (() => {
+    // check for win conditions, three in a row/tie
+    // show winning message (modal?)
+    // reset display to new game upon a button click
+    
+    // DOM 'cache'
 
-// render gameBoard display (3x3 board, 9 total squares)
+    function clearBoard() {
+        let board = document.querySelectorAll('.gameBoard-divs')
+        for (let i = 0; i < board.length; i++) {
+            board[i].textContent = ''
+        }
+    }
+    document.addEventListener('click', (e) => {
+        if (e.target.matches('#clear-gameBoard-btn')) {
+            clearBoard()
+        }
+    })
+    return {
+        clearBoard: clearBoard,
+    }
+})()
